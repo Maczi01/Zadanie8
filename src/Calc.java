8b5d3cdb900dcb39e33d97647025ed89a08e6b27
Maczi01 <system11>
2019-01-22 18:52:05 +0100

    Add scanner and change in method. Version 2

--- src/Calc.java
+++ src/Calc.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class Calc {
 
-    int calculation(int a, int b){
+    Scanner scanner = new Scanner(System.in);
+
+    int calculation(){
+        System.out.println("Podaj pierwsza liczbe: ");
+        int a = scanner.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        int b = scanner.nextInt();
         if (a>b){
             return a+b;
         }
@@ -12,3 +18,4 @@ public class Calc {
         return a*a;
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Calc calc = new Calc();
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj pierwsza liczbe: ");
-        int a = scanner.nextInt();
-        System.out.println("Podaj druga liczbe: ");
-        int b = scanner.nextInt();
-        System.out.println(calc.calculation(a,b));
+        System.out.println(calc.calculation());
     }
 }
